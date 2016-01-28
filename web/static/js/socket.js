9c7d7e69e631d65f2d44c4381d193c892061ffb5
guofei <guofei1986@gmail.com>
2016-01-28 23:47:11 +0900

    feat: change to webpack

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -3,7 +3,7 @@
 
 // To use Phoenix channels, the first step is to import Socket
 // and connect at the socket path in "lib/my_app/endpoint.ex":
-import {Socket} from "deps/phoenix/web/static/js/phoenix"
+import {Socket} from "phoenix"
 import DistinctID from "./distinct_id"
 
 let socket = new Socket("/socket", {
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+module.exports = {
+  devtool: "source-map",
+  entry: [
+    "./web/static/js/app.js",
+    "./web/static/css/app.scss"
+  ],
+  output: {
+    path: "./priv/static",
+    filename: "js/app.js"
+  },
+
+  resolve: {
+    modulesDirectories: [
+      __dirname + "/web/static/js",
+      __dirname + "/node_modules"
+    ],
+    extensions: ["", ".js"],
+    alias: {
+      phoenix_html:
+      __dirname + "/deps/phoenix_html/web/static/js/phoenix_html.js",
+      phoenix:
+      __dirname + "/deps/phoenix/web/static/js/phoenix.js"
+    }
+  },
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel",
+      query: {
+        presets: ["es2015", "react"]
+      }
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract("style", "css")
+    }, {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract("style", "css!sass")
+    }, {
+      test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/font-woff"
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/octet-stream"
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "file"
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=image/svg+xml"
+    }]
+  },
+
+  plugins: [
+    new ExtractTextPlugin("css/app.css"),
+    new CopyWebpackPlugin([{ from: "./web/static/assets" }])
+  ]
+};
