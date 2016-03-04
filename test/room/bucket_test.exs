8c7c347f34dce1568d251d5d514ce17f7d10a92a
guofei <guofei1986@gmail.com>
2016-03-04 11:24:14 +0900

    feat: test past

--- test/room/bucket_test.exs
+++ test/room/bucket_test.exs
@@ -3,9 +3,9 @@ defmodule EmbedChat.Room.BucketTest do
 
   test "stores values by key" do
     {:ok, bucket} = EmbedChat.Room.Bucket.start_link
-    assert EmbedChat.Room.Bucket.get(bucket) == nil
+    assert EmbedChat.Room.Bucket.get(bucket) == []
 
     EmbedChat.Room.Bucket.add(bucket, 1)
-    assert EmbedChat.Room.Bucket.get(bucket) == 1
+    assert EmbedChat.Room.Bucket.get(bucket) == [1]
   end
 end
