aa3581aa079944573ad47533f1777399be2538f8
guofei <guofei1986@gmail.com>
2016-03-04 11:43:57 +0900

    feat: upgrade room bucket test

--- test/room/bucket_test.exs
+++ test/room/bucket_test.exs
@@ -1,11 +1,18 @@
 defmodule EmbedChat.Room.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
+  setup do
     {:ok, bucket} = EmbedChat.Room.Bucket.start_link
+    {:ok, bucket: bucket}
+  end
+
+  test "stores values", %{bucket: bucket} do
     assert EmbedChat.Room.Bucket.get(bucket) == []
 
     EmbedChat.Room.Bucket.add(bucket, 1)
     assert EmbedChat.Room.Bucket.get(bucket) == [1]
+
+    EmbedChat.Room.Bucket.add(bucket, 2)
+    assert EmbedChat.Room.Bucket.get(bucket) == [2, 1]
   end
 end
