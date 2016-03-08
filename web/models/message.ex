f53d75b45bd0623ed5ecb3537f87850bf2d652f4
guofei <guofei1986@gmail.com>
2016-03-09 00:33:23 +0900

    fix: message belongs to address

--- web/models/message.ex
+++ web/models/message.ex
@@ -5,14 +5,14 @@ defmodule EmbedChat.Message do
     field :message_type, :string
     field :subject, :string
     field :body, :string
-    belongs_to :incomings, EmbedChat.Address
-    belongs_to :outgoings, EmbedChat.Address
+    belongs_to :incoming, EmbedChat.Address
+    belongs_to :outgoing, EmbedChat.Address
 
     timestamps
   end
 
-  @required_fields ~w(message_type subject body)
-  @optional_fields ~w()
+  @required_fields ~w(message_type body)
+  @optional_fields ~w(subject)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
