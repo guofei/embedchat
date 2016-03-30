d565fd5aeddcc23f04a095ad355f1971d6b30865
guofei <guofei1986@gmail.com>
2016-03-31 00:39:42 +0900

    feat: set deploy

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,8 @@ use Mix.Config
 config :embed_chat, EmbedChat.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: {:system, "HOST"}, port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule EmbedChat.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.10"},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 2.3"}
+     {:comeonin, "~> 2.3"},
+     {:exrm, "~> 1.0"}
     ]
   end
 
