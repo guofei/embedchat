fe0379b5688c723aa0d0b588e2963aff6c6747ba
guofei <guofei1986@gmail.com>
2016-04-04 21:49:04 +0900

    feat: change bucket to mapset

--- lib/embed_chat/room/bucket.ex
+++ lib/embed_chat/room/bucket.ex
@@ -3,27 +3,27 @@ defmodule EmbedChat.Room.Bucket do
   Starts a new bucket.
   """
   def start_link do
-    Agent.start_link(fn -> [] end)
+    Agent.start_link(fn -> MapSet.new() end)
   end
 
   @doc """
   Gets a list from the `bucket`
   """
   def get(bucket) do
-    Agent.get(bucket, &(&1))
+    Agent.get(bucket, &(MapSet.to_list(&1)))
   end
 
   @doc """
   Adds the `value` in the `bucket`.
   """
   def add(bucket, value) do
-    Agent.update(bucket, &(Enum.uniq [value|&1]))
+    Agent.update(bucket, &(MapSet.put(&1, value)))
   end
 
   @doc """
   Deletes the `value` in the `bucket`.
   """
   def delete(bucket, value) do
-    Agent.update(bucket, &(List.delete &1, value))
+    Agent.update(bucket, &(MapSet.delete &1, value))
   end
 end
