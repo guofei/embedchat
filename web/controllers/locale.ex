48c565ddb5c26b7319b8ddeeff67affc8f3a34ff
guofei <guofei1986@gmail.com>
2016-04-20 11:45:38 +0900

    feat: add locale plug

---
+++ web/controllers/locale.ex
@@ -0,0 +1,14 @@
+defmodule EmbedChat.Locale do
+  import Plug.Conn
+
+  def init(opts), do: nil
+
+  def call(conn, _opts) do
+    case conn.params["locale"] || get_session(conn, :locale) do
+      nil     -> conn
+      locale  ->
+        Gettext.put_locale(EmbedChat.Gettext, locale)
+        conn |> put_session(:locale, locale)
+    end
+  end
+end
