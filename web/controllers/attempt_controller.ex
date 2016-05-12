dcd711effd77fe1177b04d3860e86b69c1b19536
guofei <guofei1986@gmail.com>
2016-05-12 23:17:06 +0900

    feat: redirect to http in attemptcontroller

--- web/controllers/attempt_controller.ex
+++ web/controllers/attempt_controller.ex
@@ -33,12 +33,18 @@ defmodule EmbedChat.AttemptController do
 
   def show(conn, %{"id" => id}) do
     attempt = Repo.get!(Attempt, id)
-    room = Repo.get!(Room, 1)
-    source = get_source(attempt.url)
-    if source == "<html><head></head><body></body></html>" do
-      render(conn, "show.html", data: "Not found :( <br> url: #{attempt.url}", room: room)
+    if conn.scheme == :https do
+      new_conn = %Plug.Conn{conn | scheme: :http }
+      new_conn
+      |> redirect(to: attempt_path(new_conn, :show, attempt))
     else
-      render(conn, "show.html", data: source, room: room)
+      room = Repo.get!(Room, 1)
+      source = get_source(attempt.url)
+      if source == "<html><head></head><body></body></html>" do
+        render(conn, "show.html", data: "Not found :( <br> url: #{attempt.url}", room: room)
+      else
+        render(conn, "show.html", data: source, room: room)
+      end
     end
   end
 
