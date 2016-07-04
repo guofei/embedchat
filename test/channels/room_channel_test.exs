da5b277d2d0bfe6275a9fd5c683d81695a2fdf59
guofei <guofei1986@gmail.com>
2016-07-04 22:44:38 +0900

    refactor: remove unnecessary function

--- test/channels/room_channel_test.exs
+++ test/channels/room_channel_test.exs
@@ -18,16 +18,6 @@ defmodule EmbedChat.RoomChannelTest do
     end
   end
 
-  test "ping replies with status ok", %{socket: socket} do
-    ref = push socket, "ping", %{"hello" => "there"}
-    assert_reply ref, :ok, %{"hello" => "there"}
-  end
-
-  test "shout broadcasts to room", %{socket: socket} do
-    push socket, "shout", %{"hello" => "all"}
-    assert_broadcast "shout", %{"hello" => "all"}
-  end
-
   test "broadcasts are pushed to the client", %{socket: socket} do
     broadcast_from! socket, "broadcast", %{"some" => "data"}
     assert_push "broadcast", %{"some" => "data"}
--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -50,12 +50,6 @@ defmodule EmbedChat.RoomChannel do
     {:noreply, socket}
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
-
   @messages_size 50
 
   def handle_in("messages", payload, socket) do
@@ -84,13 +78,6 @@ defmodule EmbedChat.RoomChannel do
     else
       {:reply, {:ok, %{admins: RoomChannelSF.online_admins(socket.assigns.room_id)}}, socket}
     end
-  end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (rooms:lobby).
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
-    {:noreply, socket}
   end
 
   def handle_in("new_message", payload, socket) do
