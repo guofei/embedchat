46d30b57df6be2ba85e03beeecc57031432c1865
guofei <guofei1986@gmail.com>
2016-07-25 20:50:11 +0900

    fix: fix build error

--- lib/embed_chat/repo/metrics.ex
+++ lib/embed_chat/repo/metrics.ex
@@ -2,7 +2,7 @@ defmodule EmbedChat.Repo.Metrics do
   use Elixometer
 
   def record_metric(entry) do
-    update_histogram("query_exec_time", (entry.query_time + (entry.queue_time || 0) / 1000)
+    update_histogram("query_exec_time", (entry.query_time + entry.queue_time || 0) / 1000)
     update_histogram("query_queue_time", (entry.queue_time || 0) / 1000)
     update_spiral("query_count", 1)
   end
