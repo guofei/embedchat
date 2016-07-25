6b56c8a56b984c8818217009550730fc3fe7e854
guofei <guofei1986@gmail.com>
2016-07-26 00:04:20 +0900

    feat: change record_metric

--- lib/embed_chat/repo/metrics.ex
+++ lib/embed_chat/repo/metrics.ex
@@ -2,8 +2,10 @@ defmodule EmbedChat.Repo.Metrics do
   use Elixometer
 
   def record_metric(entry) do
-    update_histogram("query_exec_time", (entry.query_time + entry.queue_time || 0) / 1000)
-    update_histogram("query_queue_time", (entry.queue_time || 0) / 1000)
+    query_time = System.convert_time_unit(entry.query_time + (entry.queue_time || 0), :native, :milliseconds)
+    queue_time = System.convert_time_unit(entry.queue_time || 0, :native, :milliseconds)
+    update_histogram("query_exec_time", query_time)
+    update_histogram("query_queue_time", queue_time)
     update_spiral("query_count", 1)
   end
 end
