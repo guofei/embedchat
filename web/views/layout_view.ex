67b4b94c92ccbfc62aae3316d0f14e4be7f5d961
guofei <guofei1986@gmail.com>
2016-08-30 11:46:50 +0900

    feat: add hreflang

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -5,4 +5,9 @@ defmodule EmbedChat.LayoutView do
   def current_locale_path(conn, locale) do
     conn.request_path <> "?locale=#{locale}"
   end
+
+  # TODO support params
+  def current_locale_url(conn, locale) do
+    EmbedChat.Router.Helpers.url(conn) <> conn.request_path <> "?locale=#{locale}"
+  end
 end
