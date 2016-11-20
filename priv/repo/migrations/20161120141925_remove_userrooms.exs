f2700d332e6ca501828ac2d825341a87de1f2750
guofei <guofei1986@gmail.com>
2016-11-20 23:23:56 +0900

    feat: remove userrooms table

---
+++ priv/repo/migrations/20161120141925_remove_userrooms.exs
@@ -0,0 +1,18 @@
+defmodule EmbedChat.Repo.Migrations.RemoveUserrooms do
+  use Ecto.Migration
+
+  def up do
+    drop table(:userrooms)
+  end
+
+  def down do
+    create table(:userrooms) do
+      add :user_id, references(:users, on_delete: :delete_all)
+      add :room_id, references(:rooms, on_delete: :delete_all)
+
+      timestamps
+    end
+    create index(:userrooms, [:user_id])
+    create index(:userrooms, [:room_id])
+  end
+end
